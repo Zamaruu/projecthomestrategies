1010f33d897b150423169dc04977342665978cf7
Maximilian Ditz <max.ditz@outlook.de>
2021-12-23 15:10:34 +0100

    basic pending tasks on homepage implemented

--- lib/widgets/homepage/tasks/pendingtaskspanel.dart
+++ lib/widgets/homepage/tasks/pendingtaskspanel.dart
@@ -9,17 +9,50 @@ class PendingTasksPanel extends StatefulWidget {
 }
 
 class _PendingTasksPanelState extends State<PendingTasksPanel> {
+  late List<String> tasks;
+
+  @override
+  void initState() {
+    super.initState();
+    tasks = List.generate(3, (index) => "Aufgabe $index");
+  }
+
+  void removeTaskFromList(int index){
+    setState(() {
+      tasks.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: ListView.builder(
-        itemCount: 3,
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemBuilder: (BuildContext context, int index) {
-          return PendingTaskTile(taskName: "Aufgabe $index");
-        },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(4.0),
+            child: Text(
+              "Deine austehenden Aufgaben",
+              style: TextStyle(
+                fontSize: 17,
+              ),
+            ),
+          ),
+          ListView.builder(
+            itemCount: tasks.length,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              return PendingTaskTile(
+                key: Key(tasks[index]), //Damit die Checkboxen richtig gerendert werden wenn eine Task beendet wird! 
+                listIndex: index,
+                onTaskFinished: removeTaskFromList,
+                taskName: tasks[index],
+              );
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/homepage/tasks/pendingtasktile.dart
+++ lib/widgets/homepage/tasks/pendingtasktile.dart
@@ -2,23 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:projecthomestrategies/widgets/globalwidgets/roundcheckbox.dart';
 
 class PendingTaskTile extends StatelessWidget {
+  final int listIndex;
   final String taskName;
-  
-  const PendingTaskTile({ Key? key, required this.taskName }) : super(key: key);
+  final Function onTaskFinished;
+
+  const PendingTaskTile({ Key? key, required this.listIndex ,required this.taskName, required this.onTaskFinished }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 2,
+      elevation: 4,
       child: ListTile(
         leading: RoundCheckbox(
           initialValue: false,
           onChanged: (newValue){
-            print(newValue);
+            if(newValue){
+              onTaskFinished(listIndex);
+            }
           },
         ),
         title: Text(
           taskName,
+        ),
+        trailing: PopupMenuButton(
+          icon: Icon(Icons.more_vert, color: Colors.grey.shade700,), 
+          itemBuilder: (context) => [
+            const PopupMenuItem(
+              child: Text("Abschließen"),
+              value: 1,
+            ),
+            const PopupMenuItem(
+              child: Text("Ändern"),
+              value: 2,
+            ),
+            const PopupMenuItem(
+              child: Text("Löschen"),
+              value: 2,
+            ),
+            const PopupMenuItem(
+              child: Text("Zuweisen"),
+              value: 2,
+            ),
+          ]
         ),
       ),
     );
