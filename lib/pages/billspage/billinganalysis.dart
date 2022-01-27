fb5ce797696d78a330c54e800288e66b0cf00ded
Maximilian Ditz <max.ditz@outlook.de>
2022-01-27 14:00:12 +0100

    changed file name

--- lib/pages/billspage/billinganalysis.dart
+++ lib/pages/billspage/billinganalysis.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:projecthomestrategies/bloc/provider/analysis_state.dart';
 import 'package:projecthomestrategies/bloc/provider/billing_state.dart';
 import 'package:projecthomestrategies/widgets/billspage/analysis/barchartcategorysummary.dart';
-import 'package:projecthomestrategies/widgets/billspage/analysis/piechartcategoeysummary.dart';
+import 'package:projecthomestrategies/widgets/billspage/analysis/piechartcategorysummary.dart';
 import 'package:projecthomestrategies/widgets/billspage/analysis/resetanalysistimefilter.dart';
 import 'package:projecthomestrategies/widgets/billspage/analysis/setfilterdates.dart';
 import 'package:projecthomestrategies/widgets/billspage/analysis/thirtydayretro.dart';
