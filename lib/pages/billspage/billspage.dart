483361d0a6d531a76dfc3f4f6eea9d8e54448ef2
Maximilian Ditz <max.ditz@outlook.de>
2022-02-13 17:37:55 +0100

    bill page is now tabview

--- lib/pages/billspage/billspage.dart
+++ lib/pages/billspage/billspage.dart
@@ -12,12 +12,20 @@ class BillsPage extends StatefulWidget {
   State<BillsPage> createState() => _BillsPageState();
 }
 
-class _BillsPageState extends State<BillsPage> {
+class _BillsPageState extends State<BillsPage>
+    with SingleTickerProviderStateMixin {
   late int pageIndex;
+  late TabController _tabController;
 
   @override
   void initState() {
     pageIndex = 0;
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      setState(() {
+        pageIndex = _tabController.index;
+      });
+    });
     super.initState();
   }
 
@@ -37,30 +45,46 @@ class _BillsPageState extends State<BillsPage> {
   Widget build(BuildContext context) {
     return BaseScaffold(
       pageTitle: pageTitles.elementAt(pageIndex),
-      body: _pages.elementAt(pageIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: pageIndex,
-        onTap: (index) {
-          if (index != pageIndex) {
+      //body: _pages.elementAt(pageIndex),
+      body: TabBarView(
+        controller: _tabController,
+        children: _pages,
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(width: 1.3, color: Colors.grey[300]!),
+          ),
+        ),
+        height: kBottomNavigationBarHeight,
+        child: TabBar(
+          indicatorColor: Theme.of(context).primaryColor,
+          unselectedLabelColor: Colors.grey[600],
+          labelColor: Theme.of(context).primaryColor,
+          controller: _tabController,
+          onTap: (index) {
             setState(() {
               pageIndex = index;
             });
-          }
-        },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.euro),
-            label: "Rechnungen",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt_rounded),
-            label: "Kategorien",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.analytics),
-            label: "Analyse",
-          ),
-        ],
+          },
+          tabs: const <Tab>[
+            Tab(
+              iconMargin: EdgeInsets.only(bottom: 5),
+              text: "Rechnungen",
+              icon: Icon(Icons.euro),
+            ),
+            Tab(
+              iconMargin: EdgeInsets.only(bottom: 5),
+              text: "Kategorien",
+              icon: Icon(Icons.list_alt_rounded),
+            ),
+            Tab(
+              iconMargin: EdgeInsets.only(bottom: 5),
+              text: "Analyse",
+              icon: Icon(Icons.analytics),
+            ),
+          ],
+        ),
       ),
     );
   }
